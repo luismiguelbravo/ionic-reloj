1a9b8a90d79ef55845c24de8cd0738f309fc22f7
luismiguelbravo <lmbgott@gmail.com>
2019-05-04 22:05:43 -0400

    se crea componente para mostar el detalle

--- src/app/entrada/detalle/detalle.component.ts
+++ src/app/entrada/detalle/detalle.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
+import { Entrada } from '../../Entrada';
 
 @Component({
   selector: 'app-detalle',
@@ -7,8 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleComponent implements OnInit {
 
-  constructor() { }
+    fechaDeHoy = moment() //.format('YYYY-MM-DD HH:mm:ss');
+    finalDeLaEspera = moment("2021-01-01")
+    diferencia = null;
+    diferenciaString = "";
 
-  ngOnInit() {}
+    diferenciaEnSegundos = null;
+    diferenciaEnMinutos = null;
+    diferenciaEnHoras = null;
+    diferenciaEnDias = null;
+    diferenciaEnMeses = null;
+    diferenciaEnYears = null;
+
+    @Input() entrada: Entrada;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.ticTac();
+
+        this.finalDeLaEspera = moment(this.entrada.fecha);
+
+        console.log("");
+        console.log(" =============== this.entrada ===============");
+        console.log(this.entrada)
+        console.log(" =============== this.entrada ===============");
+        console.log("");
+    }
+
+    ticTac(): void {
+        setTimeout(() => 
+        {
+            this.calcularDiferencias();
+            this.ticTac();
+        }, 900);
+    }
+
+    calcularDiferencias(): void {
+        this.fechaDeHoy = moment()
+        this.diferenciaEnYears = this.finalDeLaEspera.diff(this.fechaDeHoy, 'years')
+        this.fechaDeHoy.add(this.diferenciaEnYears, 'years')
+
+        this.diferenciaEnMeses = this.finalDeLaEspera.diff(this.fechaDeHoy, 'months')
+        this.fechaDeHoy.add(this.diferenciaEnMeses, 'months')
+
+        this.diferenciaEnDias = this.finalDeLaEspera.diff(this.fechaDeHoy, 'days')
+        this.fechaDeHoy.add(this.diferenciaEnDias, 'days')
+
+        this.diferenciaEnHoras = this.finalDeLaEspera.diff(this.fechaDeHoy, 'hours')
+        this.fechaDeHoy.add(this.diferenciaEnHoras, 'hours')
+
+        this.diferenciaEnMinutos = this.finalDeLaEspera.diff(this.fechaDeHoy, 'minutes')
+        this.fechaDeHoy.add(this.diferenciaEnMinutos, 'minutes')
+
+        this.diferenciaEnSegundos = this.finalDeLaEspera.diff(this.fechaDeHoy, 'seconds')
+    }
 
 }
