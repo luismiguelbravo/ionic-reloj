fb5dd0e43ef759e60cc49133a538ca81d6da9bf4
Luis Bravo <lmbravo.3it@bancochile.cl>
2019-10-04 16:39:56 -0400

    se agrega plantill de traduccion y palabras utilizadas en el diccionario

---
+++ plantilla_de_traduccion.ts
@@ -0,0 +1,49 @@
+"Invalid_Title": 
+"Invalid_Year":
+"Invalid_Date":
+"Welcome": 
+"Search": 
+"Order" : 
+"Ascending order": 
+"Descending order":
+"Save": 
+"Cancel": 
+"Success": 
+"Delete" : 
+"Event_saved_successfully": 
+"Warning": 
+"This action cannot be reversed": 
+"Remove": 
+"Title": 
+"Event": 
+"Year": 
+"Month": 
+"Day": 
+"Hour": 
+"Minute": 
+"Second": 
+"Add": 
+"Select One":
+
+"Months":[
+    { "value": 0, "name": ""},
+    { "value": 1, "name": ""},
+    { "value": 2, "name": ""},
+    { "value": 3, "name": ""},
+    { "value": 4, "name": ""},
+    { "value": 5, "name": ""},
+    { "value": 6, "name": ""},
+    { "value": 7, "name": ""},
+    { "value": 8, "name": ""},
+    { "value": 9, "name": ""},
+    { "value": 10, "name": ""},
+    { "value": 11, "name":"" }
+],
+
+"Copy": 
+"Share": 
+"Accept" : 
+"Edit" : 
+"event_date": 
+"Future": 
+"Past" : 
