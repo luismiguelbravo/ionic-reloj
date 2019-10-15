dc598089c9f9ec986df49db18d0c12f98d73dd9d
Luis Bravo <lmbravo.3it@bancochile.cl>
2019-10-15 14:57:21 -0300

    se actualizan plantillas de traduccion

--- traduccion/plantilla_de_traduccion.ts
+++ traduccion/plantilla_de_traduccion.ts
@@ -24,22 +24,20 @@
 "Second": 
 "Add": 
 "Select One": 
-
 "Months":[
-    { "value": 0, "name": ""},
-    { "value": 1, "name": ""},
-    { "value": 2, "name": ""},
-    { "value": 3, "name": ""},
-    { "value": 4, "name": ""},
-    { "value": 5, "name": ""},
-    { "value": 6, "name": ""},
-    { "value": 7, "name": ""},
-    { "value": 8, "name": ""},
-    { "value": 9, "name": ""},
-    { "value": 10, "name": ""},
-    { "value": 11, "name": ""},
+    { "value": 0, "name": 
+    { "value": 1, "name": 
+    { "value": 2, "name": 
+    { "value": 3, "name": 
+    { "value": 4, "name": 
+    { "value": 5, "name": 
+    { "value": 6, "name": 
+    { "value": 7, "name": 
+    { "value": 8, "name": 
+    { "value": 9, "name": 
+    { "value": 10, "name": 
+    { "value": 11, "name": 
 ],
-
 "Copy": 
 "Share": 
 "Accept" : 
