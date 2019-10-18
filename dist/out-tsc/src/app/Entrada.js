418099dd5e1ec68bfbb1ad18ae2df41bacf7da4b
Luis Bravo <lmbravo.3it@bancochile.cl>
2019-10-18 14:09:44 -0300

    se eliminan archivos para que no sean rastreados por el git

--- dist/out-tsc/src/app/Entrada.js
+++
@@ -1,8 +0,0 @@
-var Entrada = /** @class */ (function () {
-    function Entrada() {
-    }
-    return Entrada;
-}());
-export { Entrada };
-/* Agregar campo de milisegundos. */ 
-//# sourceMappingURL=Entrada.js.map
