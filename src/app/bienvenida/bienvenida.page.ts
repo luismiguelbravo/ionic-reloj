4e9caa23e9080c2dcff35c43fc958d5ffe5b8653
Luis Miguel Bravo <lmb_gott@hotmail.com>
2019-10-25 18:29:40 -0300

    cambios en el estilo para ajustarse a las pruebas e2e

--- src/app/bienvenida/bienvenida.page.ts
+++ src/app/bienvenida/bienvenida.page.ts
@@ -11,6 +11,7 @@ export class BienvenidaPage implements OnInit {
 
     listaDeIdiomas : any;
     ididomaSeleccionado: any;
+    palabraDeBusqueda = null
 
     constructor(
         public idiomaService: IdiomaService,
@@ -32,6 +33,22 @@ export class BienvenidaPage implements OnInit {
         let vm = this;
         vm.ididomaSeleccionado = this.idiomaService.get_idioma_by_indice(idioma.indice)
     }
+
+    buscar():void {
+        let vm = this
+        if (vm.palabraDeBusqueda !== "" && vm.palabraDeBusqueda!== null)
+        {
+            vm.listaDeIdiomas = this.idiomaService.listaDeIdiomasReducidos().filter(function(idioma) {
+                console.log("");
+                console.log(" ===== idioma =====");
+                console.log(idioma);
+                console.log(" ===== idioma =====");
+                console.log("");
+                return idioma.Autoglotonimo.toLowerCase().includes(vm.palabraDeBusqueda.toLowerCase()) ||  idioma.Autoglotonimo.includes(vm.palabraDeBusqueda) 
+            });
+        }
+    }
+
 
     dismiss() {
         // using the injected ModalController this page
